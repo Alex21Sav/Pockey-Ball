db308781928febe14cd53912cdf33c141f5c28eb
Alex <savostin2133@gmail.com>
2021-08-13 22:53:04 +0700

    Study Project # 10

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Segment _segmentTemplate;
+    [SerializeField] private Block _blockTemplate;
+    [SerializeField] private Finish _finishTemplate;
+    [SerializeField] private int _towerSize;
+
+    private void Start()
+    {
+        BuildTower();
+    }
+    private void BuildTower()
+    {
+        GameObject currentPoint = gameObject;
+
+        for (int i = 0; i < _towerSize; i++)
+        {
+            currentPoint = BuilSegment(currentPoint, _segmentTemplate.gameObject);
+            currentPoint = BuilSegment(currentPoint, _blockTemplate.gameObject);
+        }
+
+        BuilSegment(currentPoint, _finishTemplate.gameObject);
+    }
+    private GameObject BuilSegment(GameObject currentSegment, GameObject nextSegment)
+    {
+        return Instantiate(nextSegment, GetbuildPoint(currentSegment.transform , nextSegment.transform), Quaternion.identity, transform);
+    }
+
+    private Vector3 GetbuildPoint(Transform currentSegment, Transform nextSegment)
+    {
+        return new Vector3(transform.position.x, currentSegment.position.y + currentSegment.localScale.y / 2 + nextSegment.localScale.y / 2, transform.position.z);
+    }
+}
